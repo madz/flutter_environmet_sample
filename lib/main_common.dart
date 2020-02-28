9d4f9b7bd885c07383bc58cb103fc372dd054dc0
madz <markdiesta@gmail.com>
2020-02-28 18:31:26 +0800

    change prod build config settings.

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -9,7 +9,7 @@ Future<void> mainCommon(String env) async {
   // Always call this if the main method is asynchronous
   WidgetsFlutterBinding.ensureInitialized();
   // Load the JSON config into memory
-  await ConfigReader.initialize();
+  await ConfigReader.initialize(env);
 
   Color primaryColor;
   switch (env) {
